aa7a1b013af6ecf97fd5ca316161336eaa62d259
Gueffier Romain <r.gueffier@gmail.com>
2021-02-16 19:19:27 +0100

    Bug role utilisateur par défaut, correction graphique

--- symfony/src/Entity/User.php
+++ symfony/src/Entity/User.php
@@ -113,7 +113,7 @@ class User implements UserInterface
 
     public function setRoles(array $roles): self
     {
-        $this->roles = $roles;
+        $this->roles = $roles ?: ['ROLE_USER'];
 
         return $this;
     }
